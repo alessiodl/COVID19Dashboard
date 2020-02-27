8679b4ee661d611d84f58159dbc94d42a44f4390
Alessio Di Lorenzo <alessio.dl@gmail.com>
2020-02-27 10:56:45 +0100

    App base structure

---
+++ index.js
@@ -0,0 +1,33 @@
+import '@fortawesome/fontawesome-free/js/all';
+
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import {fromLonLat, transform} from 'ol/proj';
+import {Tile as TileLayer, Image as ImageLayer} from 'ol/layer';
+import {OSM} from 'ol/source';
+
+import axios from 'axios';
+
+import 'nouislider/distribute/nouislider.min.css';
+import noUiSlider from 'nouislider'
+import moment from 'moment';
+
+const data_url = "https://covid19-it-api.herokuapp.com/data";
+
+// *******************************************
+// Map
+// *******************************************
+var map = new Map({
+    target: 'map',
+    layers: [
+      new TileLayer({
+        source: new OSM()
+      })
+    ],
+    view: new View({
+      center: fromLonLat([14, 42]),
+      zoom: 6
+    })
+  });
+  
